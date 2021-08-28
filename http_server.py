e07b97d8dc8b0366e65d2a6d93e1dbed6d542c35
rotohands <rotohands@gmail.com>
2021-08-28 21:00:32 +0300

    http server locally works

--- http_server.py
+++ http_server.py
@@ -10,17 +10,17 @@ class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
     # handle GET command
     def do_GET(self):
         try:
-            if self.path.endswith('/new'):
-                # send code 200 response
-                self.send_response(200)
-                msg = main().encode()
+
+            # send code 200 response
+            self.send_response(200)
+            msg = main().encode()
 
                 # send header first
-                self.send_header('Content-type', msg)
-                self.end_headers()
+            self.send_header('Content-type', msg)
+            self.end_headers()
 
                 # send file content to client
-                return
+            return
 
         except IOError:
             self.send_error(404, 'file not found')
