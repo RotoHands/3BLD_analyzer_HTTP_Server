fc850b06619cfb03634cab885d192a8ecadf7676
rotohands <rotohands@gmail.com>
2021-09-04 12:47:41 +0300

    working on do_post

--- http_client.py
+++ http_client.py
@@ -1,14 +1,45 @@
 import http.client
 import json
+import requests
+import re
 
-conn = http.client.HTTPSConnection('127.0.0.1:8080')
+def load_letter_pairs_dict(path_letter_pair):
+    """
+    gets a text file with list of place in cube and its letter pair ("ULB" : "A").
+    returns a dictionary object of { place : letter_pair }
+    """
 
-headers = {'Content-type': 'application/json'}
+    with open(path_letter_pair, "r", encoding="utf-8") as f:
+        dict_lp = {}
+        file = f.readlines()
+        for line in file:
+            split_data = re.findall('"([^"]*)"', line)
+            dict_lp[split_data[0]] = split_data[1]
+    return (dict_lp)
 
-foo = {'text': 'Hello HTTP #1 **cool**, and #1!'}
-json_data = json.dumps(foo)
 
-conn.request('POST', '/post', json_data, headers)
+with open("example_solves/example_smart_cube.txt", "r") as f:
+    data = f.readlines()
+    SCRAMBLE = data[0]
+    SOLVE = " ".join(data[1:])
 
-response = conn.getresponse()
-print(response.read().decode())
+letter_pair_dict = load_letter_pairs_dict("sticker_HEB.txt")
+memo_time = 23.23
+params_solve = {"DIFF" : 0.89,
+          "SMART_CUBE": True,
+          "COMMS_UNPARSED" :False,
+          "E_BUF" : "UF",
+          "C_BUF" : "UFR",
+          "LP": True ,
+          "MOVE_COUNT" : True,
+          "LETTER_PAIRS" : letter_pair_dict.__str__(),
+          "CUBEDB" : True,
+          "NAME_OF_SOLVE" : "example_smart_cube",
+          "TIME_SOLVE" : 56.12,
+          "SCRAMBLE" : SCRAMBLE,
+          "SOLVE" : SOLVE,
+          "MEMO" : memo_time
+}
+# r = requests.post("http://rotohands-bld-parser.herokuapp.com/", data=params_solve)
+r = requests.post("127.0.0.1:8080", data=params_solve)
+print(r.text)
--- http_server.py
+++ http_server.py
@@ -5,11 +5,14 @@ from main import parse
 
 # Create custom HTTPRequestHandler class
 
-PORT = os.environ['PORT']
+# PORT = os.environ['PORT']
 
-class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
+class S(BaseHTTPRequestHandler):
 
-    # handle GET command
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
     def do_GET(self):
         try:
 
@@ -21,6 +24,16 @@ class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
         except IOError:
             self.send_error(404, 'file not found')
 
+    def do_POST(self):
+
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        print("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
 
 def run():
     print('http server is starting...')
@@ -28,7 +41,8 @@ def run():
     # ip and port of servr
     # by default http server port is 80
     server_address = ('0.0.0.0', int(PORT))
-    httpd = http.server.HTTPServer(server_address, KodeFunHTTPRequestHandler)
+    server_address = ('127.0.0.1', 8080)
+    httpd = http.server.HTTPServer(server_address, S)
     print('http server is running...')
     httpd.serve_forever()
 
